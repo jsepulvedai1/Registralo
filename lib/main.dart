0468b5701b0c2b26aca5acbbea20a0b662e19ef7
Sofia <spizarror@correo.uss.cl>
2020-01-10 12:49:20 -0300

    cargandoregistralo

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 
-import 'package:disenos/src/pages/basico_page.dart';
-import 'package:disenos/src/pages/scroll_page.dart';
+// import 'package:disenos/src/pages/basico_page.dart';
+// import 'package:disenos/src/pages/scroll_page.dart';
 import 'package:disenos/src/pages/botones_page.dart';
+import 'package:disenos/src/pages/SignInOne.dart';
 
 
 void main() => runApp(MyApp());
@@ -22,11 +23,12 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Diseños',
-      initialRoute: 'basico',
+      initialRoute: 'botones',
       routes: {
-        'basico'  : (BuildContext context ) => BasicoPage(),
-        'scroll'  : (BuildContext context ) => ScrollPage(),
+        // 'basico'  : (BuildContext context ) => BasicoPage(),
+        // 'scroll'  : (BuildContext context ) => ScrollPage(),
         'botones' : (BuildContext context ) => BotonesPage(),
+        'login'  : (BuildContext context) =>   SignInOne(),
       },
     );
   }
--- lib/src/pages/botones_page.dart
+++ lib/src/pages/botones_page.dart
@@ -1,36 +1,86 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_swiper/flutter_swiper.dart';
 import 'dart:math';
 import 'dart:ui';
 
+class BotonesPage extends StatefulWidget {
+  BotonesPage({Key key}) : super(key: key);
 
-class BotonesPage extends StatelessWidget {
-  
+  @override
+  _BotonesPage createState() => _BotonesPage();
+}
+
+class _BotonesPage extends State<BotonesPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: <Widget>[
-          _fondoApp(),
 
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Bienvenido '),
+        actions: <Widget>[
+            // action button
+            IconButton(
+              icon: Icon(Icons.person_outline),
+              onPressed: ()=> Navigator.pushNamed(context, 'login'),
+            ),
+        ]
+      ),
+      body: 
+      Stack(
+     
+      children: <Widget>[
+         _fondoApp(),
+        
           SingleChildScrollView(
             child: Column(
               children: <Widget>[
-                _titulos(),
+
+                
+                _swiperTarjetas(), 
+                _pagina2(),  
+                //_titulos(),
                 _botonesRedondeados()
               ],
-            ),
-          )
+              ),
+          ),
+          
+        // Container (
+        //   child: Column(
+        //   children: <Widget>[
+        //   _swiperTarjetas()
+        //     ],
+        //     )
+        // ),
 
-        ],
+      ],
       ),
-      bottomNavigationBar: _bottomNavigationBar(context)
+  
+    
+     
+
     );
   }
+  Widget _pagina2() {
+  return SafeArea(
+    child: Container(
+      padding: EdgeInsets.all(20.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text('Registralo.cl', style: TextStyle( color: Colors.white, fontSize: 30.0, fontWeight: FontWeight.bold )),
+          SizedBox( height: 20.0 ),
+          Text('Para registrar bienes y consultar mayor detalle de bienes registrados en el sistema, debes inscribirte, completando en el icono REGISTRATE,', style: TextStyle( color: Colors.white, fontSize: 18.0 )),
+        ],
+      ),
+    ),
+  );
+}
+
+
 
 
   Widget _fondoApp(){
-
     final gradiente = Container(
       width: double.infinity,
       height: double.infinity,
@@ -39,14 +89,12 @@ class BotonesPage extends StatelessWidget {
           begin: FractionalOffset(0.0, 0.6),
           end: FractionalOffset(0.0, 1.0),
           colors: [
-            Color.fromRGBO(52, 54, 101, 1.0),
-            Color.fromRGBO(35, 37, 57, 1.0)
+            Color.fromRGBO(148, 196, 254, 1.0),
+            Color.fromRGBO(0, 132, 170, 1.0)
           ]
         )
       ),
     );
-
-
     final cajaRosa = Transform.rotate(
       angle: -pi / 5.0,
       child: Container(
@@ -72,91 +120,98 @@ class BotonesPage extends StatelessWidget {
         )
       ],
     );
-
   }
+    Widget _swiperTarjetas(){
+    return Container(
+      width: double.infinity,
+      height: 170.0,
+   
+   
+      child: new Swiper(
+        itemBuilder: (BuildContext context , int index){
+          return Image.network('https://registralo.spd.gov.cl/images/banner1.png', 
+          fit: BoxFit.fill);
+          
 
-  Widget _titulos() {
+        },
+       
+      
+        itemCount: 2,
+        pagination: new SwiperPagination(),
+        control: new SwiperControl(),
+      
 
-    return SafeArea(
-      child: Container(
-        padding: EdgeInsets.all(20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text('Registralo', style: TextStyle( color: Colors.white, fontSize: 30.0, fontWeight: FontWeight.bold )),
-            SizedBox( height: 20.0 ),
-            Text('Classify this transaction into a particular category', style: TextStyle( color: Colors.white, fontSize: 18.0 )),
-          ],
-        ),
       ),
     );
-
+    
   }
-
-  Widget _bottomNavigationBar(BuildContext context) {
-
-    return Theme(
-      data: Theme.of(context).copyWith(
-        canvasColor: Color.fromRGBO(55, 57, 84, 1.0),
-        primaryColor: Colors.pinkAccent,
-        textTheme: Theme.of(context).textTheme
-          .copyWith( caption: TextStyle( color: Color.fromRGBO(116, 117, 152, 1.0) ) )
-      ),
-      child: BottomNavigationBar(
+  // Widget _titulos() {
+  //   return SafeArea(
+  //     child: Container(
+  //       padding: EdgeInsets.all(20.0),
+  //       child: Column(
+  //         crossAxisAlignment: CrossAxisAlignment.start,
+  //         children: <Widget>[
+  //           Text('Registralro', style: TextStyle( color: Colors.white, fontSize: 30.0, fontWeight: FontWeight.bold )),
+  //           SizedBox( height: 20.0 ),
+  //           Text('Para registrar bienes y consultar mayor detalle de bienes registrados en el sistema, debes inscribirte, completando en el icono REGISTRATE,', style: TextStyle( color: Colors.white, fontSize: 18.0 )),
+  //         ],
+  //       ),
+  //     ),
+  //   );
+  // }
+  // Widget _bottomNavigationBar(BuildContext context) {
+  //   return Theme(
+  //     data: Theme.of(context).copyWith(
+  //       canvasColor: Color.fromRGBO(200, 164, 214, 1.0),
+  //       primaryColor: Colors.pinkAccent,
+  //       textTheme: Theme.of(context).textTheme
+  //         .copyWith( caption: TextStyle( color: Color.fromRGBO(116, 117, 152, 1.0) ) )
+  //     ),
+  //     child: BottomNavigationBar(
         
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon( Icons.calendar_today, size: 30.0 ),
-            title: Container()
-          ),
-          BottomNavigationBarItem(
-            icon: Icon( Icons.bubble_chart, size: 30.0 ),
-            title: Container()
-          ),
-          BottomNavigationBarItem(
-            icon: Icon( Icons.supervised_user_circle, size: 30.0 ),
-            title: Container()
-          ),
-        ],
-      ),
-    );
-
-  }
-
-
+  //       items: [
+  //         BottomNavigationBarItem(
+  //           icon: Icon( Icons.calendar_today, size: 30.0 ),
+  //           title: Container()
+  //         ),
+  //         BottomNavigationBarItem(
+  //           icon: Icon( Icons.bubble_chart, size: 30.0 ),
+  //           title: Container()
+  //         ),
+  //         BottomNavigationBarItem(
+  //           icon: Icon( Icons.supervised_user_circle, size: 30.0 ),
+  //           title: Container()
+  //         ),
+  //       ],
+  //     ),
+  //   );
+  // }
   Widget _botonesRedondeados() {
-
+   
     return Table(
       children: [
         TableRow(
           children: [
-            _crearBotonRedondeado( Colors.blue, Icons.border_all, 'Consultar bien' ),
-            _crearBotonRedondeado( Colors.purpleAccent, Icons.directions_bus, 'Bus' ),
-          ]
+            _crearBotonRedondeado( Colors.blue, Icons.search, 'Consultar bien' ),
+            _crearBotonRedondeado( Colors.green, Icons.info_outline, 'Consejos de prevención' ),
+            
+            
+          ],
+        
+          
         ),
         TableRow(
           children: [
-            _crearBotonRedondeado( Colors.pinkAccent, Icons.shop, 'consejos de prevencion' ),
-            _crearBotonRedondeado( Colors.orange, Icons.insert_drive_file, 'File' ),
+            _crearBotonRedondeado( Colors.orange, Icons.mail, 'Contacto' ),
+            _crearBotonRedondeado( Colors.pink, Icons.help_outline, '¿Cómo funciona?' ),
           ]
         ),
-        TableRow(
-          children: [
-            _crearBotonRedondeado( Colors.blueAccent, Icons.movie_filter, 'Contacto' ),
-            _crearBotonRedondeado( Colors.green, Icons.cloud, 'Grocery' ),
-          ]
-        ),
-        TableRow(
-          children: [
-            _crearBotonRedondeado( Colors.red, Icons.collections, 'Photos' ),
-            _crearBotonRedondeado( Colors.teal, Icons.help_outline, 'General' ),
-          ]
-        )
+        
+        
       ],
     );
-
   }
-
   Widget _crearBotonRedondeado( Color color, IconData icono, String texto ) {
  
  
@@ -167,7 +222,7 @@ class BotonesPage extends StatelessWidget {
           height: 180.0,
           margin: EdgeInsets.all(15.0),
           decoration: BoxDecoration(
-            color: Color.fromRGBO(62, 66, 107, 0.7),
+            color: Color.fromRGBO(202, 237, 255, 0.7),
             borderRadius: BorderRadius.circular(20.0)
           ),
           child: Column(
@@ -188,6 +243,4 @@ class BotonesPage extends StatelessWidget {
       ),
     );
   }
-
-
 }
