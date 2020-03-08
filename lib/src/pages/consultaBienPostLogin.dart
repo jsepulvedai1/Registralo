f225a6f2525e22f6784caeed5a54a251f9b07a05
Sofia <spizarror@correo.uss.cl>
2020-03-08 01:28:17 -0300

    cambios en navbarPostlogin

--- lib/src/pages/consultaBienPostLogin.dart
+++ lib/src/pages/consultaBienPostLogin.dart
@@ -5,24 +5,26 @@ class ConsultaBienPostLogin  extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color.fromRGBO(140, 208, 242, 1),
-   
-      appBar: AppBar(
   
-          flexibleSpace: Image(
-          image: AssetImage('assets/consultarBienPostLogin.png'), 
-          fit: BoxFit.cover,
-          ),
-          backgroundColor: Color.fromRGBO(140, 208, 242, 1),
-
-        
-      ),
       body: Container(   
-        
         child: Column(
           children: <Widget>[
+            _crearImagen(context),
          
           ],) ,
           )
     );
+  } 
+  Widget _crearImagen(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: GestureDetector(
+        onTap: ()=> Navigator.pushNamed(context, 'scroll'),
+        child: 
+        Image.asset('assets/consultaBienPostLogin.png',height: 95.0, 
+          fit: BoxFit.contain,),
+        ),
+      );
   }
+
 }
--- lib/src/pages/misDatos.dart
+++ lib/src/pages/misDatos.dart
@@ -3,9 +3,26 @@ import 'package:flutter/material.dart';
 class MisDatos  extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('Mis Datos', textScaleFactor: 2.0),
-      
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(140, 208, 242, 1),
+   
+      body: Container(   
+        child: Column(
+          children: <Widget>[
+          _crearImagen(context),
+          ],) ,
+          )
     );
+  }
+    Widget _crearImagen(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: GestureDetector(
+        onTap: ()=> Navigator.pushNamed(context, 'scroll'),
+        child: 
+        Image.asset('assets/misdatos.png',height: 100.0, 
+          fit: BoxFit.contain,),
+        ),
+      );
   }
 }
