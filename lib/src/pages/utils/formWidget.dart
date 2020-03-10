3a4278d34623fb0481a60958fbd2919c19e19f3a
josseed <jose.zunigahenriquez@gmail.com>
2020-03-09 23:35:22 -0300

    dynamic inputs

--- lib/src/pages/utils/formWidget.dart
+++ lib/src/pages/utils/formWidget.dart
@@ -16,20 +16,39 @@ class _FormWidgetState extends State<FormWidget> {
   final ctl_address = TextEditingController();
   final ctl_city = TextEditingController();
   List<String> _categories;
-  Map<String, dynamic> brands; 
+  Map<String, dynamic> brands;
   List<String> _currentBrands;
+  List<String> _estimatedValues;
+  Map<String, List<String>> _identifiers;
+  Map<String, String> currentIdentifiers;
+  String estimatedValue;
   bool showData;
-
+  bool checkBoxBrand;
+  bool showInputs;
   @override
   void initState() {
     var categories = ['Autos', 'Notebooks'];
-
+    checkBoxBrand = false;
+    showInputs = false;
     var mapData = new Map<String, dynamic>();
+    var mapIdentifiers = new Map<String, List<String>>();
+    mapIdentifiers['Autos'] = ['Patente', 'Kilometraje'];
+    mapIdentifiers['Notebooks'] = ['MAC', 'procesador'];
     mapData['Autos'] = ['suzuki', 'kia'];
     mapData['Notebooks'] = ['hp', 'huawei'];
+    var estimatedValues = [
+      "Menor a \$100.000",
+      "\$100.000 - \$500.000",
+      "\$500.000 - \$100.0000",
+      "\$Superior a \$1000.000"
+    ];
     showData = false;
     setState(() {
+      _identifiers = mapIdentifiers;
+
       _categories = categories;
+      _estimatedValues = estimatedValues;
+      estimatedValue = estimatedValues[0];
       brands = mapData;
       _currentBrands = ['suzuki', 'kia'];
       showData = true;
@@ -55,10 +74,123 @@ class _FormWidgetState extends State<FormWidget> {
     });
   }
 
-  String dropdownValue = 'Autos';
+  Widget checkbox(String title, bool boolValue) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(title),
+        Checkbox(
+          value: boolValue,
+          onChanged: (bool value) {
+            checkBoxBrand = value;
+            setState(() {
+              checkBoxBrand = value;
+            });
+          },
+        )
+      ],
+    );
+  }
+  //List _listings = new List();
+
+  List<Widget> _getInputs(BuildContext context) {
+    List inputs = new List<Widget>();
+    List listings = _getListings(context, currentCategory);
+    if (showInputs) {
+      inputs.add(Row(mainAxisSize: MainAxisSize.min, children: <Widget>[
+      Text("Marca"),
+      Expanded(child: checkbox("Nuevo", checkBoxBrand)),
+      Expanded(
+          child: DropdownButtonFormField<String>(
+        value: dropDownValue2,
+        icon: Icon(Icons.arrow_downward),
+        iconSize: 24,
+        elevation: 16,
+        style: TextStyle(color: Colors.deepPurple),
+        onChanged: (String newValue) {
+          setState(() {
+            dropDownValue2 = newValue;
+          });
+        },
+        items: _currentBrands.map<DropdownMenuItem<String>>((String value) {
+          return DropdownMenuItem<String>(
+            value: value,
+            child: Text(value),
+          );
+        }).toList(),
+      ))
+    ]));
+    inputs.add(Row(mainAxisSize: MainAxisSize.min, children: <Widget>[
+      Expanded(
+        child: Text("Valor estimado"),
+      ),
+      Expanded(
+          child: DropdownButton<String>(
+        value: estimatedValue,
+        icon: Icon(Icons.arrow_downward),
+        iconSize: 24,
+        elevation: 16,
+        style: TextStyle(color: Colors.deepPurple),
+        underline: Container(
+          height: 2,
+          color: Colors.deepPurpleAccent,
+        ),
+        onChanged: (String newValue) {
+          setState(() {
+            estimatedValue = newValue;
+          });
+        },
+        items: _estimatedValues.map<DropdownMenuItem<String>>((String value) {
+          return DropdownMenuItem<String>(
+            value: value,
+            child: Text(value),
+          );
+        }).toList(),
+      ))
+    ]));
+    inputs.add( ConstrainedBox(
+      constraints: BoxConstraints(maxHeight: 1000),
+      child: ListView(
+        children: listings,
+      )) 
+    );
+    } else {
+      inputs.add(Text("Seleccione una categoría para más opciones"));
+    }
+    return inputs;
+  }
+
+  List<Widget> _getListings(BuildContext context, String category) {
+    // <<<<< Note this change for the return type
+    List listings = new List<Widget>();
+    if (showInputs) {
+
+      int i = 0;
+      int lenght = _identifiers[category].length;
+      for (i = 0; i < lenght; i++) {
+        var text = _identifiers[category][i];
+        listings.add(new Row(
+           mainAxisSize: MainAxisSize.min,
+           children: <Widget>[
+            Expanded(
+              child: Text(text),
+            ),
+            Expanded(
+                child: TextFormField(
+              controller: ctl_address,
+              decoration: const InputDecoration(labelText: 'Ingrese valores'),
+            ))
+          ])
+        );
+      }
+    }
+    return listings;
+  }
+
+  String currentCategory = 'Autos';
   String dropDownValue2 = 'suzuki';
   Column formOneBuilder(BuildContext context) {
-    return Column(
+    return new Column(
       children: <Widget>[
         Padding(
           padding: const EdgeInsets.all(8.0),
@@ -68,46 +200,45 @@ class _FormWidgetState extends State<FormWidget> {
               width: double.infinity,
               child: Text("Cuentanos que tipo de bien deseas registrar")),
         ),
-        DropdownButtonFormField<String>(
-          value: dropdownValue,
-          icon: Icon(Icons.arrow_downward),
-          iconSize: 24,
-          style: TextStyle(color: Colors.deepPurple),
-          
-          onChanged: (String newValue) {
-            setState(() {
-              dropdownValue = newValue;
-              _currentBrands = brands[newValue];
-              dropDownValue2 = brands[newValue][0];
-            });
-          },
-          items: _categories.map<DropdownMenuItem<String>>((String value) {
-            return DropdownMenuItem<String>(
-              value: value,
-              child: Text(value),
-            );
-          }).toList(),
-        ),
-        DropdownButtonFormField<String>(
-          value: dropDownValue2,
-          icon: Icon(Icons.arrow_downward),
-          iconSize: 24,
-          style: TextStyle(color: Colors.deepPurple),
-          onChanged: (String newValue) {
-            setState(() {
-              dropDownValue2 = newValue;
-            });
-          },
-          items: _currentBrands.map<DropdownMenuItem<String>>((String value) {
-            return DropdownMenuItem<String>(
-              value: value,
-              child: Text(value),
-            );
-          }).toList(),
-        ),
+        Row(mainAxisSize: MainAxisSize.min, children: <Widget>[
+          Expanded(
+            child: Text("Categoría"),
+          ),
+          Expanded(
+              child: DropdownButton<String>(
+            value: currentCategory,
+            icon: Icon(Icons.arrow_downward),
+            iconSize: 24,
+            elevation: 16,
+            style: TextStyle(color: Colors.deepPurple),
+            underline: Container(
+              height: 2,
+              color: Colors.deepPurpleAccent,
+            ),
+            onChanged: (String newValue) {
+              setState(() {
+                currentCategory = newValue;
+                _currentBrands = brands[newValue];
+                dropDownValue2 = brands[newValue][0];
+                showInputs = true;
+              });
+            },
+            items: _categories.map<DropdownMenuItem<String>>((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value),
+              );
+            }).toList(),
+          ))
+        ]),
+        Expanded(child: ListView(
+            shrinkWrap: true,
+            physics: ClampingScrollPhysics(),
+            children: _getInputs(context)
+          )),
         Center(
           child: Padding(
-            padding: const EdgeInsets.all(8.0),
+            padding: const EdgeInsets.all(1.0),
             child: Row(
               crossAxisAlignment: CrossAxisAlignment.center,
               mainAxisAlignment: MainAxisAlignment.center,
@@ -142,15 +273,21 @@ class _FormWidgetState extends State<FormWidget> {
           padding: const EdgeInsets.all(8.0),
           child: Container(
             padding: const EdgeInsets.all(8.0),
-            decoration: BoxDecoration(border: Border.all(color: Colors.red)),
+            decoration: BoxDecoration(border: Border.all(color: Colors.blue)),
             width: double.infinity,
-            child: Text("STEP 2"),
+            child: Text("Agrega más información de tu bien"),
           ),
         ),
-        TextFormField(
-          controller: ctl_address,
-          decoration: const InputDecoration(labelText: 'Step 2 Address'),
-        ),
+        Row(mainAxisSize: MainAxisSize.min, children: <Widget>[
+          Expanded(
+            child: Text("Selecciona algo"),
+          ),
+          Expanded(
+              child: TextFormField(
+            controller: ctl_address,
+            decoration: const InputDecoration(labelText: 'Step 2 Address'),
+          ))
+        ]),
         TextFormField(
           controller: ctl_city,
           decoration: const InputDecoration(labelText: 'Step 2 City'),
@@ -198,7 +335,9 @@ class _FormWidgetState extends State<FormWidget> {
 
         case 2:
           return Form(
-            child: (_currentBrands != null) ? this.formTwoBuilder(context) : CircularProgressIndicator(),
+            child: (_currentBrands != null)
+                ? this.formTwoBuilder(context)
+                : CircularProgressIndicator(),
           );
           break;
       }
@@ -214,5 +353,42 @@ class _FormWidgetState extends State<FormWidget> {
     ctl_name.dispose();
 
     super.dispose();
+  }
+}
+
+class RowFormBuilder extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => RowFormBuilderState();
+
+  RowFormBuilder({this.categories});
+  final List<String> categories;
+}
+
+class RowFormBuilderState extends State<RowFormBuilder> {
+  @override
+  Widget build(BuildContext context) {
+    String firstCategory = widget.categories[0];
+    return Row(
+      children: <Widget>[
+        DropdownButton<String>(
+          value: firstCategory,
+          icon: Icon(Icons.arrow_downward),
+          iconSize: 24,
+          style: TextStyle(color: Colors.deepPurple),
+          onChanged: (String newValue) {
+            setState(() {
+              firstCategory = newValue;
+            });
+          },
+          items:
+              widget.categories.map<DropdownMenuItem<String>>((String value) {
+            return DropdownMenuItem<String>(
+              value: value,
+              child: Text(value),
+            );
+          }).toList(),
+        )
+      ],
+    );
   }
 }
